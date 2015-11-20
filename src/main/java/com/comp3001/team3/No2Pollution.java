7e619fcee300d30ec26f9adeceba6a37edbf405f
mark lloyd <mark.g.n.lloyd@gmail.com>
2015-11-20 12:45:26 +0000

    for tomcat to work

--- src/main/java/com/comp3001/team3/No2Pollution.java
+++ src/main/java/com/comp3001/team3/No2Pollution.java
@@ -2,12 +2,20 @@ package com.comp3001.team3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 /**
  * Created by Anthony on 9/10/2015.
  */
 @SpringBootApplication
-public class No2Pollution {
+public class No2Pollution  extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(No2Pollution.class);
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(No2Pollution.class, args);
